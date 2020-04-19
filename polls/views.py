d7e6e2d66dab178f9524f9560268d209af668130
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-19 13:29:17 -0400

    polls/views: redef detail with get_object_or_404 shortcut

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -17,10 +17,7 @@ def index(request):
 
 def detail(request, question_id):
 
-    try:
-        question = Question.objects.get(id=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
 
     context = {
         "question": question,

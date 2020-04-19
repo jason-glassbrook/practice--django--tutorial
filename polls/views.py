5b05902262bac93421df1009da6753ac1a554df1
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-19 12:04:21 -0400

    polls/views: def vote

--- polls/views.py
+++ polls/views.py
@@ -11,3 +11,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")

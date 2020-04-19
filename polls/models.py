b474fb802decffa32d37807ee96c2683597a7c39
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-18 22:31:25 -0400

    polls/models: write Question model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(
+        "the text of the question",
+        max_length=200,
+    )
+    datetime_published = models.DateTimeField(
+        "the datetime when the question was published",
+    )

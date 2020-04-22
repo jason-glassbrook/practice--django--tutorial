3a20ecdc54049aed32157e60eb30f278d762acc6
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-22 14:09:19 -0400

    polls/tests: test was_published_recently with recent datetime_published

--- polls/tests.py
+++ polls/tests.py
@@ -19,3 +19,17 @@ class QuestionModelTests(TestCase):
         self.assertIs(future_question.was_published_recently(), False)
 
         return
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        datetime_published is within the last day.
+        """
+
+        recent_time = timezone.now() - datetime.timedelta(
+            hours=23, minutes=50, seconds=59
+        )
+        recent_question = Question(datetime_published=recent_time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+        return

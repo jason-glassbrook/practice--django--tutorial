b2824036acd59078d2f57748d1b0401c4ba59242
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-22 14:20:08 -0400

    polls/tests: test was_published_recently with old datetime_published

--- polls/tests.py
+++ polls/tests.py
@@ -23,13 +23,30 @@ class QuestionModelTests(TestCase):
     def test_was_published_recently_with_recent_question(self):
         """
         was_published_recently() should return True for questions whose
-        datetime_published is within the last day.
+        datetime_published is within 1 day.
         """
 
         recent_time = timezone.now() - datetime.timedelta(
-            hours=23, minutes=50, seconds=59
+            hours=23,
+            minutes=50,
+            seconds=59,
         )
         recent_question = Question(datetime_published=recent_time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+        return
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        datetime_published is older than 1 day.
+        """
+
+        old_time = timezone.now() - datetime.timedelta(
+            days=1,
+            seconds=1,
+        )
+        old_question = Question(datetime_published=old_time)
+        self.assertIs(old_question.was_published_recently(), False)
 
         return

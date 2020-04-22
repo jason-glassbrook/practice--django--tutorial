8ecc538b19a9e856718ca217fbd729a27fee14c8
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-22 18:37:27 -0400

    polls/tests: def helper funs

--- polls/tests.py
+++ polls/tests.py
@@ -5,6 +5,53 @@ from django.utils import timezone
 
 from .models import Question
 
+########################################
+#   TOOLS
+########################################
+
+
+def create_question(text, **kwargs):
+    """
+    Create a question with the given `text` and published with
+    the `datetime.timedelta` offset specified by `**kwargs`.
+    """
+
+    datetime_published = timezone.now() + datetime.timedelta(**kwargs)
+
+    return Question.objects.create(
+        text=text,
+        datetime_published=datetime_published,
+    )
+
+
+def tag_text(base_text, tag=None):
+
+    if tag is not None:
+        return f"{base_text} [{tag}]"
+
+    else:
+        return base_text
+
+
+def past_question_text(tag=None):
+
+    return tag_text("When is the past?", tag)
+
+
+def future_question_text(tag=None):
+
+    return tag_text("When is the future?", tag)
+
+
+def mock_question_repr(text):
+
+    return f"<Question: {text}>"
+
+
+########################################
+#   Question > Model
+########################################
+
 
 class QuestionModelTests(TestCase):
 
@@ -16,6 +63,7 @@ class QuestionModelTests(TestCase):
 
         future_time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(datetime_published=future_time)
+
         self.assertIs(future_question.was_published_recently(), False)
 
         return
@@ -28,6 +76,7 @@ class QuestionModelTests(TestCase):
 
         recent_time = timezone.now() - datetime.timedelta(days=1, seconds=-1)
         recent_question = Question(datetime_published=recent_time)
+
         self.assertIs(recent_question.was_published_recently(), True)
 
         return
@@ -40,6 +89,7 @@ class QuestionModelTests(TestCase):
 
         old_time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(datetime_published=old_time)
+
         self.assertIs(old_question.was_published_recently(), False)
 
         return

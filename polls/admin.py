47946d1518f043706e6d1b3f3613533321d32d3d
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-23 17:39:46 -0400

    polls/admin: def ChoiceInline to show Choices within QuestionAdmin

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
 
     fieldsets = [
@@ -14,6 +20,9 @@ class QuestionAdmin(admin.ModelAdmin):
         }),
     ]
 
+    inlines = [
+        ChoiceInline,
+    ]
+
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
